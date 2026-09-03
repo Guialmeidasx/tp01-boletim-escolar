db68cd9c3829d6520ea38953018484250b8dbea1
Jessica Maria Cruz de Araujo <Jessica.maria.araujo07@gmail.com>
2026-09-03 17:11:37 -0300

    compartilhado: adicionado fonte de notas

--- src/Principal.java
+++ src/Principal.java
@@ -1,29 +1,40 @@
 import java.util.Scanner;
+
 public class Principal {
-public static void main(String[] args) {
-Scanner entrada = new Scanner(System.in);
-int opcao;
-do {
-exibirCabecalho(); // <== METODO COMPARTILHADO (ponto de conflito)
-exibirMenu();
-opcao = entrada.nextInt();
-switch (opcao) {
-case 1: /* funcionalidade do Desenvolvedor A */ break;
-case 2: /* funcionalidade do Desenvolvedor B */ break;
-case 0: System.out.println("Encerrando..."); break;
-default: System.out.println("Opcao invalida!");
-}
-} while (opcao != 0);
-entrada.close();
-}
-public static void exibirCabecalho() {
-System.out.println("==============================");
-System.out.println(" NOME DO SISTEMA ");
-System.out.println("==============================");
-}
-public static void exibirMenu() {
-System.out.println("1 - ...");
-System.out.println("0 - Sair");
-System.out.print("Escolha: ");
-}
+    public static double[] notas = new double[30];
+    int quantidade;
+    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int opcao;
+        do {
+            exibirCabecalho(); // <== METODO COMPARTILHADO (ponto de conflito)
+            exibirMenu();
+            opcao = entrada.nextInt();
+            switch (opcao) {
+                case 1:
+                    /* funcionalidade do Desenvolvedor A */ break;
+                case 2:
+                    /* funcionalidade do Desenvolvedor B */ break;
+                case 0:
+                    System.out.println("Encerrando...");
+                    break;
+                default:
+                    System.out.println("Opcao invalida!");
+            }
+        } while (opcao != 0);
+        entrada.close();
+    }
+
+    public static void exibirCabecalho() {
+        System.out.println("==============================");
+        System.out.println(" NOME DO SISTEMA ");
+        System.out.println("==============================");
+    }
+
+    public static void exibirMenu() {
+        System.out.println("1 - ...");
+        System.out.println("0 - Sair");
+        System.out.print("Escolha: ");
+    }
 }
